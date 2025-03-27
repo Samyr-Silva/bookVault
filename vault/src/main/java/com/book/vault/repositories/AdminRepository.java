570cc8128fa87f19602f7aef4554b918e439e346
samyr silva <ssamyrsilva.santana@gmail.com>
2025-03-27 11:35:50 +0000

    ropositories done

---
+++ vault/src/main/java/com/book/vault/repositories/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.book.vault.repositories;
+
+import com.book.vault.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+}
---
+++ vault/src/main/java/com/book/vault/repositories/ReaderRepository.java
@@ -0,0 +1,7 @@
+package com.book.vault.repositories;
+
+import com.book.vault.model.Reader;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReaderRepository extends JpaRepository<Reader, Long> {
+}
